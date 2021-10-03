0ad5ea410d7ec7a8d5ccabde10b685896ef4dbec
aronhawkins-4 <ahawki18@asu.edu>
2021-10-03 15:04:53 -0500

    Update

--- routes/myScript.js
+++ routes/myScript.js
@@ -6,32 +6,32 @@ function countWord() {
     var spacCount360 = 360;
 
     // Get the input text value
-    var engWords90 = document.getElementById("alertEn90");
-    var engWords360 = document.getElementById("alertEn360");
-    var spanWords90 = document.getElementById("alertSp90");
-    var spanWords360 = document.getElementById("alertSp360");
+    var engChars90 = document.getElementById("alertEn90");
+    var engChars360 = document.getElementById("alertEn360");
+    var spanChars90 = document.getElementById("alertSp90");
+    var spanChars360 = document.getElementById("alertSp360");
     var chars;
     var charsLeft;
     var counter;
     var alert;
 
-    if (engWords90 === document.activeElement) {
-        chars = engWords90.value.length;
+    if (engChars90 === document.activeElement) {
+        chars = engChars90.value.length;
         charsLeft = 90;
         counter = document.getElementById("wordsLeftEn90");
         alert = document.querySelector('#alertEn90');
-    } else if (engWords360 === document.activeElement) {
-        chars = engWords360.value.length;
+    } else if (engChars360 === document.activeElement) {
+        chars = engChars360.value.length;
         charsLeft = 360;
         counter = document.getElementById("wordsLeftEn360");
         alert = document.querySelector('#alertEn360');
-    } else if (spanWords90 === document.activeElement) {
-        chars = spanWords90.value.length;
+    } else if (spanChars90 === document.activeElement) {
+        chars = spanChars90.value.length;
         charsLeft = 90;
         counter = document.getElementById("wordsLeftSp90");
         alert = document.querySelector('#alertSp90');
-    } else if (spanWords360 === document.activeElement) {
-        chars = spanWords360.value.length;
+    } else if (spanChars360 === document.activeElement) {
+        chars = spanChars360.value.length;
         charsLeft = 360;
         counter = document.getElementById("wordsLeftSp360");
         alert = document.querySelector('#alertSp360');
