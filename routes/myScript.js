b9d4df3b414ae2101bde66f19a10abe1cffc47d3
aronhawkins-4 <ahawki18@asu.edu>
2021-10-07 23:35:21 -0500

    Removed 90 char text fields. Fixed 360 Spanish text field character counter

--- routes/myScript.js
+++ routes/myScript.js
@@ -1,35 +1,21 @@
 function countWord() {
 
-    var engCount90 = 90;
     var engCount360 = 360;
-    var spanCount90 = 90;
     var spacCount360 = 360;
 
     // Get the input text value
-    var engChars90 = document.getElementById("alertEn90");
     var engChars360 = document.getElementById("alertEn360");
-    var spanChars90 = document.getElementById("alertSp90");
     var spanChars360 = document.getElementById("alertSp360");
     var chars;
     var charsLeft;
     var counter;
     var alert;
 
-    if (engChars90 === document.activeElement) {
-        chars = engChars90.value.length;
-        charsLeft = 90;
-        counter = document.getElementById("wordsLeftEn90");
-        alert = document.querySelector('#alertEn90');
-    } else if (engChars360 === document.activeElement) {
+    if (engChars360 === document.activeElement) {
         chars = engChars360.value.length;
         charsLeft = 360;
         counter = document.getElementById("wordsLeftEn360");
         alert = document.querySelector('#alertEn360');
-    } else if (spanChars90 === document.activeElement) {
-        chars = spanChars90.value.length;
-        charsLeft = 90;
-        counter = document.getElementById("wordsLeftSp90");
-        alert = document.querySelector('#alertSp90');
     } else if (spanChars360 === document.activeElement) {
         chars = spanChars360.value.length;
         charsLeft = 360;
