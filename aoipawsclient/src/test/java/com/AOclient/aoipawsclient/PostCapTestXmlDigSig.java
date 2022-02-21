895aadd0d8f430d6687f0aad72c47fec5be3b867
John Kalafus <jkalafus@asu.edu>
2022-02-21 07:16:10 -0500

    setup new test case

---
+++ aoipawsclient/src/test/java/com/AOclient/aoipawsclient/PostCapTestXmlDigSig.java
@@ -0,0 +1,17 @@
+package com.AOclient.aoipawsclient;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PostCapTestXmlDigSig {
+
+	@Test
+	public void test() {
+		String destnSignedXmlFilePath = "";
+		String privateKeyFilePath = "";
+		String publicKeyFilePath = "";
+		
+	}
+
+}
