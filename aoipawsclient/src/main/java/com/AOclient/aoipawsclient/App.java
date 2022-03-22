42985db8b7a4e39a43e4715c0997753c88831763
John Kalafus <jkalafus@asu.edu>
2022-03-22 16:34:29 -0400

    make change to app to account for new setup

--- aoipawsclient/src/main/java/com/AOclient/aoipawsclient/App.java
+++ aoipawsclient/src/main/java/com/AOclient/aoipawsclient/App.java
@@ -6,9 +6,9 @@ public class App
     public static void main( String[] args )
     {
         App app = new App();
-        if(args[0].equals("getCog") ){
+       
         app.getCog();
-        }
+        
     }
     
     private void getCog() {
