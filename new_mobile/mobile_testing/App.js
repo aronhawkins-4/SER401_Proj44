cccf4bafcd597521aff286e8f6488b9536a816a1
aronhawkins-4 <ahawki18@asu.edu>
2022-03-26 16:51:51 -0500

    Fix home screen margin bug

--- new_mobile/mobile_testing/App.js
+++ new_mobile/mobile_testing/App.js
@@ -12,16 +12,16 @@ import React, { useState } from "react";
 import CloseButton from "./CloseButton";
 
 export default function App() {
-  onNavigationStateChange = (navState) => {
-    console.log("navState", navState);
-  };
+  // onNavigationStateChange = (navState) => {
+  //   console.log("navState", navState);
+  // };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {Platform.OS === "android" && <CloseButton />}
       <WebView
         // PUT YOUR OWN COMPUTER'S IP ADDRESS BELOW
-        source={{ uri: "http://192.168.0.52:3000/" }}
+        source={{ uri: "http://192.168.68.112:3000/" }}
         // ref = {"webView"}
         // onNavigationStateChange={this.onNavigationStateChange}
       ></WebView>
