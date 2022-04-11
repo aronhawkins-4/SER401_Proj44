604a887b00696a1a5114e3ef85b7515bc2267def
John Kalafus <jkalafus@asu.edu>
2022-04-11 15:50:55 -0400

    fix client directory

--- aoipawsclient/aoipawsclient/src/generated/services/ipaws/fema/gov/capresponse/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://gov.fema.ipaws.services/capresponse", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package services.ipaws.fema.gov.capresponse;
--- aoipawsclient/aoipawsclient/src/generated/services/ipaws/fema/gov/ipaws_capservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://gov.fema.ipaws.services/IPAWS_CAPService/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package services.ipaws.fema.gov.ipaws_capservice;
